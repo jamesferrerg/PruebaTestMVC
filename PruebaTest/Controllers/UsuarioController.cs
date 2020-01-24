16df87e8f1c8ca7838af2759c7cd512216087996
jamesferrerg <46761770+jamesferrerg@users.noreply.github.com>
2020-01-23 22:20:45 -0500

    Practica sencilla de mvc

---
+++ PruebaTest/Controllers/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using PruebaTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaTest.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            List < UsuarioCLS > listaUsuarios = null;
+            try
+            {
+                using (var bd = new BDPruebaTecnicaEntities())
+                {
+                    listaUsuarios = (from item in bd.Usuarios
+                                     where item.Habilitado == 1
+                                     select new UsuarioCLS
+                                     {
+                                         nombre = item.Nombre,
+                                         apellido = item.Apellido,
+                                         correo = item.Email,
+                                         celular = (long)item.Celular,
+                                         telefono = (int)item.Telefono
+                                     }).ToList();
+                }
+        }
+            catch (Exception ex)
+            {
+
+                return View(listaUsuarios);
+    }
+
+            return View(listaUsuarios);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UsuarioCLS oUsuarioCls)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oUsuarioCls);
+            }
+            else
+            {
+                using(var bd=new BDPruebaTecnicaEntities())
+                {
+                    Usuarios oUsuario = new Usuarios();
+                    oUsuario.Nombre = oUsuarioCls.nombre;
+                    oUsuario.Apellido = oUsuarioCls.apellido;
+                    oUsuario.Email = oUsuarioCls.correo;
+                    oUsuario.Celular = oUsuarioCls.celular;
+                    oUsuario.Telefono = oUsuarioCls.telefono;
+                    oUsuario.Habilitado = 1;
+                    bd.Usuarios.Add(oUsuario);
+                    bd.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PruebaTest/Models/UsuarioCLS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTest.Models
+{
+    public class UsuarioCLS
+    {
+        [Required]
+        [StringLength(100, ErrorMessage ="Longitud maxima de 100 caracteres")]
+        public string nombre { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage ="Longitud maxima de 100 caracteres")]
+        public string apellido { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage ="Longitud maxima de 100 caracteres")]
+        public string correo { get; set; }
+
+        [Required(ErrorMessage ="Número de celular es requerido")]
+        public long celular { get; set; }
+
+        public int telefono { get; set; }
+
+        public int habilitado { get; set; }
+    }
+}
